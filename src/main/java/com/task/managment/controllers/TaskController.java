82943c215df87294261cb6e6750930f1b2924514
islam-mansour <islammansour06@gmail.com>
2024-01-03 05:27:49 +0100

    Adding thymeleaf templates for crud

--- src/main/java/com/task/managment/controllers/TaskController.java
+++ src/main/java/com/task/managment/controllers/TaskController.java
@@ -3,14 +3,14 @@ package com.task.managment.controllers;
 import com.task.managment.exciptions.RecordNotFoundException;
 import com.task.managment.model.Task;
 import com.task.managment.service.GoogleCalenderService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.CacheEvict;
-import org.springframework.cache.annotation.CachePut;
-import org.springframework.cache.annotation.Cacheable;
-import org.springframework.cache.annotation.Caching;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -20,7 +20,7 @@ import com.task.managment.service.TaskService;
 import java.io.IOException;
 import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping("/tasks")
 public class TaskController {
 
@@ -30,14 +30,12 @@ public class TaskController {
     @Autowired
     GoogleCalenderService googleCalenderService;
 
-    @Autowired
-    GoogleCalenderController googleCalenderController;
 
     @GetMapping
-    public ResponseEntity<List<Task>> getAllTasks() {
-        List<Task> list = taskService.getAllTasks();
-
-        return new ResponseEntity<List<Task>>(list, new HttpHeaders(), HttpStatus.OK);
+    public String getAllTasks(Model model) {
+        List<Task> tasks = taskService.getAllTasks();
+        model.addAttribute("tasks", tasks);
+        return "index";
     }
 
     @GetMapping("/{id}")
@@ -48,24 +46,45 @@ public class TaskController {
         return new ResponseEntity<Task>(entity, new HttpHeaders(), HttpStatus.OK);
     }
 
+
+    @GetMapping("/add")
+    public String showAddTaskForm(Task task) {
+        return "add-task";
+    }
     @PostMapping
-    public ResponseEntity<Task> createTask(@RequestBody Task task) throws IOException {
+    public String createTask(@Valid Task task, BindingResult result, Model model) throws IOException {
+        if (result.hasErrors()){
+            return "add-task";
+        }
         Task newTask = taskService.createTask(task);
         googleCalenderService.queueTask(task);
-        return new ResponseEntity<Task>(task, new HttpHeaders(), HttpStatus.OK);
+        return "redirect:/tasks";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String showUpdateForm(@PathVariable("id") long id, Model model) throws RecordNotFoundException {
+        Task task = taskService.getTaskById(id);
+
+        model.addAttribute("task", task);
+        return "update-task";
     }
 
     @PutMapping
-    public ResponseEntity<Task> updateTask(@RequestBody Task task)
+    public String updateTask(@Valid Task task, BindingResult result, Model model)
             throws RecordNotFoundException {
+
+        if (result.hasErrors()) {
+            return "update-task";
+        }
+
         Task updatedTask = taskService.updateTask(task);
-        return new ResponseEntity<Task>(updatedTask, new HttpHeaders(), HttpStatus.OK);
+        return "redirect:/tasks";
     }
 
-    @DeleteMapping("/{id}")
-    public HttpStatus deleteTaskById(@PathVariable("id") Long id)
+    @GetMapping("/delete/{id}")
+    public String deleteTaskById(@PathVariable("id") Long id)
             throws RecordNotFoundException {
         taskService.deleteTaskById(id);
-        return HttpStatus.OK;
+        return "redirect:/tasks";
     }
 }
